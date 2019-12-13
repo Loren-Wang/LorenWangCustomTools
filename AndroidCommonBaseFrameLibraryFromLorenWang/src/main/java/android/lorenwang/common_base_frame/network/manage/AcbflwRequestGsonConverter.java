52d6774bb0d028b70aa07dffcbfab63cc43c8a19
Loren-Wang <wangliang_workjob@163.com>
2019-12-13 23:51:07 +0800

    修改工具类文件类型

---
+++ AndroidCommonBaseFrameLibraryFromLorenWang/src/main/java/android/lorenwang/common_base_frame/network/manage/AcbflwRequestGsonConverter.java
@@ -0,0 +1,52 @@
+package android.lorenwang.common_base_frame.network.manage;
+
+import android.lorenwang.tools.base.AtlwLogUtils;
+
+import com.google.gson.Gson;
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonWriter;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+import okhttp3.MediaType;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import okio.Buffer;
+import retrofit2.Converter;
+
+/**
+ * 功能作用：
+ * 创建时间：2019-12-13 下午 22:24:18
+ * 创建人：王亮（Loren wang）
+ * 思路：
+ * 方法：
+ * 注意：
+ * 修改人：
+ * 修改时间：
+ * 备注：
+ */
+
+public class AcbflwRequestGsonConverter<T> implements Converter<T, RequestBody> {
+    private Gson gson;
+    private TypeAdapter<T> adapter;
+    private MediaType MEDIA_TYPE = MediaType.parse("application/json; charset=UTF-8");
+    private Charset UTF_8 = StandardCharsets.UTF_8;
+
+    public AcbflwRequestGsonConverter(Gson gson, TypeAdapter<T> adapter) {
+        this.gson = gson;
+        this.adapter = adapter;
+    }
+
+    @Override
+    public RequestBody convert(T value) throws IOException {
+        Buffer buffer = new Buffer();
+        OutputStreamWriter writer = new OutputStreamWriter(buffer.outputStream(), UTF_8);
+        JsonWriter jsonWriter = gson.newJsonWriter(writer);
+        adapter.write(jsonWriter, value);
+        jsonWriter.close();
+        return RequestBody.create(MEDIA_TYPE, buffer.readByteString());
+    }
+}
