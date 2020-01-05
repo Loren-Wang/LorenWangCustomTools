25913f03a541bd55b452403d6450fc05c4ea7aa4
Loren-Wang <wangliang_workjob@163.com>
2020-01-05 16:12:00 +0800

    修改安卓基础框架，使其可以访问http请求数据

--- AndroidCommonBaseFrameLibraryFromLorenWang/src/main/java/android/lorenwang/common_base_frame/network/AcbflwNetworkManager.kt
+++ AndroidCommonBaseFrameLibraryFromLorenWang/src/main/java/android/lorenwang/common_base_frame/network/AcbflwNetworkManager.kt
@@ -2,9 +2,11 @@ package android.lorenwang.common_base_frame.network
 
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import android.lorenwang.common_base_frame.network.manage.AcbflwResponseGsonConverterFactory
+import okhttp3.ConnectionSpec
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
+import java.util.*
 import java.util.concurrent.TimeUnit
 
 /**
@@ -43,14 +45,19 @@ open class AcbflwNetworkManager private constructor() {
      * @param baseUrl 基础url链接
      * @param interceptor 网络拦截器
      */
-    fun initRetrofit(baseUrl: String, interceptor: Interceptor) {
-        val client = OkHttpClient.Builder()
-                .addInterceptor(interceptor)
-                .readTimeout(30, TimeUnit.SECONDS)
-                .build()
+    fun initRetrofit(baseUrl: String, interceptor: Interceptor?) {
+        var builder = OkHttpClient.Builder()
+        //防止无法进行http请求
+        builder = builder.connectionSpecs(Arrays.asList(ConnectionSpec.MODERN_TLS, ConnectionSpec.COMPATIBLE_TLS, ConnectionSpec.CLEARTEXT))
+        //设置超时时间
+        builder = builder.readTimeout(30, TimeUnit.SECONDS)
+        //添加拦截器
+        interceptor?.let {
+            builder = builder.addInterceptor(it)
+        }
         lwRetrofit = Retrofit.Builder()
                 .baseUrl(baseUrl)
-                .client(client)
+                .client(builder.build())
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .addConverterFactory(AcbflwResponseGsonConverterFactory.create()) //使用自定义的gson数据解析器，部分代码取自源码当中的gson解析器
                 .build()
