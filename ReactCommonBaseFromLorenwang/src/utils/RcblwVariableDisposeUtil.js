4c98bf5a1f47322188571c2469eca6d29fd839df
LorenWang <wl@qtoolsbaby.com>
2020-03-13 14:38:18 +0800

    测试提交

---
+++ ReactCommonBaseFromLorenwang/src/utils/RcblwVariableDisposeUtil.js
@@ -0,0 +1,15 @@
+/**
+ * 功能作用：变量管理工具
+ * 初始注释时间： 2020/3/13 14:33
+ * 注释创建人：LorenWang（王亮）
+ * 方法介绍：
+ * 思路：
+ * 修改人：
+ * 修改时间：
+ * 备注：
+ */
+export default class RcblwVariableDisposeUtil {
+    test(){
+        console.log('hello world!');
+    }
+}
