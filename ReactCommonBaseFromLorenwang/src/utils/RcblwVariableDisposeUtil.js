70e9338327a7a137b33f17a3c921ccda35b120b8
LorenWang <wl@qtoolsbaby.com>
2020-03-13 19:32:58 +0800

    暂推

--- ReactCommonBaseFromLorenwang/src/utils/RcblwVariableDisposeUtil.js
+++ ReactCommonBaseFromLorenwang/src/utils/RcblwVariableDisposeUtil.js
@@ -8,8 +8,38 @@
  * 修改时间：
  * 备注：
  */
-export default class RcblwVariableDisposeUtil {
-    test(){
-        console.log('hello world!');
+const RcblwVariableDisposeUtil = {
+    /**
+     * 获取参数类型
+     * @param params 参数
+     * @returns  参数类型
+     */
+    getParamsType(params) {
+        return Object.prototype.toString.call(params).constructor
+    },
+    /**
+     * 判断参数是否是字符串
+     * @param params 参数
+     * @returns {boolean} 是否是字符串，true为是
+     */
+    paramsIsString(params) {
+        return jstlwGetParamsType(params) === String
+    },
+    /**
+     * 判断参数是否为空
+     * @param params 参数
+     * @return boolean|boolean 空为true
+     */
+    paramsIsEmpty(params) {
+        return params == null || this.getParamsType(params) === undefined || (this.paramsIsString(params) && params === "")
+    },
+    /**
+     * 判断参数是否为空字符串或者空
+     * @param params 参数
+     * @return boolean|boolean 空为true
+     */
+    paramsIsEmptyStr(params) {
+        return this.paramsIsEmpty(params) || (this.paramsIsString(params) && params.length === 0)
     }
-}
+};
+export default RcblwVariableDisposeUtil
