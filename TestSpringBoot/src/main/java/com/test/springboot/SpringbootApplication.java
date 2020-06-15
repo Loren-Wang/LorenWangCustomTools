d56b8c7c2fe8252616808aa7e19766c9eca55eda
“LorenWang” <wangliang_workjob@163.com>
2020-06-15 12:28:09 +0800

    修改modal配置

--- TestSpringBoot/src/main/java/com/test/springboot/SpringbootApplication.java
+++ TestSpringBoot/src/main/java/com/test/springboot/SpringbootApplication.java
@@ -3,6 +3,18 @@ package com.test.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 功能作用：测试SpringBoot入口
+ * 初始注释时间： 2020/6/15 12:27 下午
+ * 注释创建人：LorenWang（王亮）
+ * 方法介绍：
+ * 思路：
+ * 修改人：
+ * 修改时间：
+ * 备注：
+ *
+ * @author LorenWang（王亮）
+ */
 @SpringBootApplication
 public class SpringbootApplication {
 
