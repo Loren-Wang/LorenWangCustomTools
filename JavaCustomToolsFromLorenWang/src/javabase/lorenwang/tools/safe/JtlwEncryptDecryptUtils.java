bc7b84cb7d171e58ec3310927a4853bb259c76ba
“LorenWang” <wangliang_workjob@163.com>
2020-12-11 18:28:07 +0800

    JavaCustomToolsFromLorenWang:::修复java加密解密异常

--- JavaCustomToolsFromLorenWang/src/javabase/lorenwang/tools/safe/JtlwEncryptDecryptUtils.java
+++ JavaCustomToolsFromLorenWang/src/javabase/lorenwang/tools/safe/JtlwEncryptDecryptUtils.java
@@ -11,18 +11,29 @@ import javabase.lorenwang.tools.common.JtlwCheckVariateUtils;
 
 /**
  * 功能作用：加密解密工具类
- * 创建时间：2020-01-05 下午 22:43:16
- * 创建人：王亮（Loren wang）
+ * 初始注释时间： 2020/12/11 6:24 下午
+ * 创建人：王亮（Loren）
  * 思路：
  * 方法：
  * 注意：
  * 修改人：
  * 修改时间：
  * 备注：
+ *
+ * @author 王亮（Loren）
  */
-
 public class JtlwEncryptDecryptUtils {
     private static volatile JtlwEncryptDecryptUtils optionUtils;
+
+    /**
+     * 加密方式
+     */
+    private final String KEY_ALGORITHM = "AES";
+
+    /**
+     * 默认的加密算法
+     */
+    private final String DEFAULT_CIPHER_ALGORITHM = "AES/CBC/PKCS5Padding";
 
     /**
      * 私有构造
@@ -55,12 +66,13 @@ public class JtlwEncryptDecryptUtils {
                 || JtlwCheckVariateUtils.getInstance().isEmpty(ivs)) {
             return "";
         }
+
         try {
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-            SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
+            Cipher cipher = Cipher.getInstance(DEFAULT_CIPHER_ALGORITHM);
+            SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), KEY_ALGORITHM);
             IvParameterSpec ivParameterSpec = new IvParameterSpec(ivs.getBytes());
             cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivParameterSpec);
-            return new String(cipher.doFinal(Base64.getEncoder().encode(srcData.getBytes())), StandardCharsets.UTF_8);
+            return Base64.getEncoder().encodeToString(cipher.doFinal(srcData.getBytes()));
         } catch (Exception e) {
             return "";
         }
@@ -81,11 +93,12 @@ public class JtlwEncryptDecryptUtils {
             return "";
         }
         try {
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-            SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(StandardCharsets.US_ASCII), "AES");
+            Cipher cipher = Cipher.getInstance(DEFAULT_CIPHER_ALGORITHM);
+            SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), KEY_ALGORITHM);
             IvParameterSpec ivParameterSpec = new IvParameterSpec(ivs.getBytes());
-            cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivParameterSpec);
-            return new String(cipher.doFinal(Base64.getDecoder().decode(srcData.getBytes())), StandardCharsets.UTF_8);
+            cipher.init(Cipher.DECRYPT_MODE, keySpec, ivParameterSpec);
+            byte[] bytes = Base64.getDecoder().decode(srcData);
+            return new String(cipher.doFinal(bytes));
         } catch (Exception e) {
             return "";
         }
---
+++ JavaCustomToolsFromLorenWang/src/test/java/javabase/lorenwang/tools/safe/JtlwEncryptDecryptUtilsTest.java
@@ -0,0 +1,39 @@
+package javabase.lorenwang.tools.safe;
+
+import org.junit.Test;
+
+import javabase.lorenwang.tools.dataConversion.JtlwDecimalConvertUtils;
+
+import static org.junit.Assert.*;
+
+/**
+ * 功能作用：
+ * 创建时间：2020-12-11 5:49 下午
+ * 创建人：王亮（Loren）
+ * 思路：
+ * 方法：
+ * 注意：
+ * 修改人：
+ * 修改时间：
+ * 备注：
+ *
+ * @author 王亮（Loren）
+ */
+public class JtlwEncryptDecryptUtilsTest {
+
+    @Test
+    public void encrypt() {
+        System.out.println(JtlwEncryptDecryptUtils.getInstance().encrypt(
+                "c57d7de31cfd446393f9181349a93cf8-251f21ba-WEB-1607679461987",
+                "6pcWBA9PyapDv3Q1", "6pcWBA9PyapDv3Q1"
+        ));
+    }
+
+    @Test
+    public void decrypt() {
+        System.out.println(JtlwEncryptDecryptUtils.getInstance().decrypt(
+                "eSdiPHrCw70GwCK3DcsbIjLcwbYUhMqNmY6g1aV2u43UbqO8EThc+WF655xISYXDVZN8zTMFOultZtGRKc2CzQ==",
+                "6pcWBA9PyapDv3Q1", "6pcWBA9PyapDv3Q1"
+        ));
+    }
+}
